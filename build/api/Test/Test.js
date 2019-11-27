b95c895cb573eaae537d3b22daa330420c56c343
James Jung <leedo01219@gmail.com>
2019-11-27 12:02:03 -0500

    Test deployment

---
+++ build/api/Test/Test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  Query: {
+    test: function test() {
+      return "Hello";
+    }
+  }
+};
+exports["default"] = _default;
